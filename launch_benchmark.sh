27f05292046abef09c3594d74d7f42e511f52c66
dujun <jun.du@intel.com>
2022-12-02 03:44:41 +0000

    add launch_benchmark.sh

---
+++ launch_benchmark.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+set -xe
+
+function main {
+    source common.sh
+    # set common info
+    init_params $@
+    fetch_device_info
+    set_environment
+
+    # requirements
+    pip uninstall fairseq -y
+    pip install -e .
+
+    cp -r ${DATASET_DIR} . 
+    # if multiple use 'xxx,xxx,xxx'
+    model_name_list=($(echo "${model_name}" |sed 's/,/ /g'))
+    batch_size_list=($(echo "${batch_size}" |sed 's/,/ /g'))
+
+    # generate benchmark
+    for model_name in ${model_name_list[@]}
+    do
+        #
+        for batch_size in ${batch_size_list[@]}
+        do
+            # clean workspace
+            logs_path_clean
+
+            # generate launch script for multiple instance
+            if [ "${OOB_USE_LAUNCHER}" == "1" ] && [ "${device}" == "cpu" ];then
+                generate_core_launcher
+            else
+                generate_core
+            fi
+            # launch
+            echo -e "\n\n\n\n Running..."
+            source ${excute_cmd_file}
+            echo -e "Finished.\n\n\n\n"
+            # collect launch result
+            collect_perf_logs
+        done
+    done
+}
+
+# run
+function generate_core {
+    # generate multiple instance script
+    for(( i=0; i<instance; i++ ))
+    do
+        real_cores_per_instance=$(echo ${device_array[i]} |awk -F, '{print NF}')
+        log_file="${log_dir}/rcpi${real_cores_per_instance}-ins${i}.log"
+
+        # instances
+        if [ "${device}" == "cpu" ];then
+            OOB_EXEC_HEADER=" numactl -m $(echo ${device_array[i]} |awk -F ';' '{print $2}') "
+            OOB_EXEC_HEADER+=" -C $(echo ${device_array[i]} |awk -F ';' '{print $1}') "
+        elif [ "${device}" == "cuda" ];then
+            OOB_EXEC_HEADER=" CUDA_VISIBLE_DEVICES=${device_array[i]} "
+	    if [[ "${mode_name}" == "realtime" ]];then
+	        addtion_options+=" --nv_fuser "
+	    fi
+	elif [[ "${mode_name}" == "train" && "${device}" == "xpu" ]];then
+	    OOB_EXEC_HEADER=" IPEX_XPU_ONEDNN_LAYOUT=0 "
+	fi
+	# remove jit, longformers fail with "ValueError: not enough values to unpack (expected 2, got 1)"
+        printf " ${OOB_EXEC_HEADER} \
+	    python train.py \
+	    	data-bin/iwslt14.tokenized.de-en --optimizer nag --lr 0.25 --clip-norm 0.1 \
+		--dropout 0.2 --arch fconv_iwslt_de_en --save-dir checkpoints/fconv \
+		--batch-size ${batch_size} \
+	    	--num_iters $num_iter --num_warmup $num_warmup \
+		--channels_last $channels_last --precision $precision \
+		--device ${device} \
+                ${addtion_options} \
+        > ${log_file} 2>&1 &  \n" |tee -a ${excute_cmd_file}
+        if [ "${numa_nodes_use}" == "0" ];then
+            break
+        fi
+    done
+    echo -e "\n wait" >> ${excute_cmd_file}
+}
+
+function generate_core_launcher {
+    # generate multiple instance script
+    for(( i=0; i<instance; i++ ))
+    do
+        real_cores_per_instance=$(echo ${device_array[i]} |awk -F, '{print NF}')
+        log_file="${log_dir}/rcpi${real_cores_per_instance}-ins${i}.log"
+
+        printf "python -m launch --enable_jemalloc \
+                    --core_list $(echo ${device_array[@]} |sed 's/;.//g') \
+                    --log_file_prefix rcpi${real_cores_per_instance} \
+                    --log_path ${log_dir} \
+                    --ninstances ${#device_array[@]} \
+                    --ncore_per_instance ${real_cores_per_instance} \
+            tools/infer.py --weights $CKPT_DIR \
+                --source $DATASET_DIR \
+                --num_iter $num_iter --num_warmup $num_warmup \
+                --channels_last $channels_last --precision $precision \
+                ${addtion_options} \
+        > /dev/null 2>&1 &  \n" |tee -a ${excute_cmd_file}
+        break
+    done
+    echo -e "\n wait" >> ${excute_cmd_file}
+    # download launcher
+    wget --no-proxy -O launch.py http://mengfeil-ubuntu.sh.intel.com/share/launch.py
+}
+
+# download common files
+timeout 10s wget -q -O common.sh https://raw.githubusercontent.com/mengfei25/oob-common/gpuoob/common.sh || cp /tmp/common.sh .
+
+# Start
+main "$@"
